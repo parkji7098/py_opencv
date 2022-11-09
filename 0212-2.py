cf99f502b66407ef3fe43e062a68de4e7522513c
parkji7098 <wjddlszzzzzzzzzzz11@gmail.com>
2022-11-09 12:58:04 +0900

    update

--- 0212-2.py
+++ 0212-2.py
@@ -1,46 +1,119 @@
+#0212-2.py
+
+from copy import deepcopy
 import cv2
+import time
 
 class Animator():
-    def __init__(self, initFunc, updateFrameFunc, interval):
+
+    def __init__(self, winname, initFunc, updateFrameFunc, stopFunc, pauseFunc, interval):
+        self.__winname = winname
+        
         self.__initFunc = initFunc
         self.__updateFrameFunc = updateFrameFunc
+        self.__stopFunc = stopFunc
+        self.__pauseFunc = pauseFunc
         self.__interval = interval
-
-    def show(self):
-        # 처음으로 초기화 함수를 호출
-        self.__initFunc()
-
-        # 루프 지정된 간격으로 프레임을 갱신하는 함수를 호출
-        while True:
-            self.__updateFrameFunc()
-            key = cv2.waitKey(self.__interval)
-            if key == 27:
-                break
         
 
+        # print('__updateFrameFunc: ', self.__updateFrameFunc)
 
 
+    def show(self):
+
+        # 처음으로 한번만 동작하는 초기화 함수를 호출
+        self.__initFunc(self.__winname)
+
+        # 루프, 지정된 간격으로 프레임을 갱신하는 함수를 호출
+        frameCount = 1
+        while True:
+            # print('self: ', self)
+            retval = self.__updateFrameFunc(self.__winname, frameCount)
+
+            # if not retval:
+            #     break
+
+            # interval의 값만큼 지연
+            # time.sleep(self.__interval / 1000)
+            key = cv2.waitKey(self.__interval)
+            
+            if key == 27: #esc
+                # 영상 정지 호출
+                self.__stopFunc(self.__winname)
+                break
+
+            if key == 32: #스페이스
+                # 영상 일시정시
+                self.__pauseFunc(self.__winname)
+
+                while True:
+                    key = cv2.waitKey()
+                    if key == 32:
+                        break
+
+            frameCount += 1
+            pass
+
+        pass
+
+
+
+def videoInit(winname):
+
+    retval, frame = cap.read()
+
+    # 여기에 문구를 합성
+    cv2.putText(frame, text="READY", org=(50,50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,0,0), thickness=2)
+    cv2.imshow(winname, frame)
+
+
+currentFrame = None
+
+def updateFrame(winname, k):
+    print('k: ', k)
+    retval, frame = cap.read()
+    
+    global currentFrame
+    currentFrame = frame.copy()
+
+    if retval:
+        cv2.putText(frame, text="PLAY", org=(50,50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,0,0), thickness=2)
+        cv2.imshow(winname, frame)        
+
+    return retval
+
+def videoStop(winname):
+
+    frame = currentFrame.copy()
+    # frame = deepcopy(currentFrame)
+
+    cv2.putText(frame, text="STOP", org=(50,50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,0,0), thickness=2)
+    cv2.imshow(winname, frame)
+
+    cv2.waitKey()
+    pass
+
+def videoPause(winname):
+
+    # TODO 현재프레임을 읽어올 방법이 필요!
+    frame = currentFrame.copy()
+
+    cv2.putText(frame, text="PAUSE", org=(50,50), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255,0,0), thickness=2)
+    cv2.imshow(winname, frame)
+
+    pass
 
 
 cap = cv2.VideoCapture('./data/vtest.avi')
-def init():
-    retval, frame = cap.read()
-    cv2.imshow('video', frame)
 
-def updateFrame():
-    retval, frame = cap.read()
-    if retval:
-        cv2.imshow('video', frame)
-
-retval, frame = cap.read()
-cv2.imshow('video', frame)
+# print('updateFrame: ', updateFrame)
 
 # 콜백함수를 연결
-ani = Animator(init, updateFrame, 50)
+ani = Animator('video1', videoInit, updateFrame, videoStop, videoPause, 50)
 
 # show, 재생시작
-ani.show()
+ani.show() #블럭
+
 
 if cap.isOpened():
     cap.release()
-
--- 0311.py
+++ 0311.py
@@ -1,29 +1,70 @@
+#0311.py
 import cv2
 import numpy as np
 
+isDoubleClick = False
+
 def onMouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if flags & cv2.EVENT_FLAG_SHIFTKEY:
-            cv2.rectangle(param[0], (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0)) 
 
+    global isDoubleClick
+
+    # print('flags: ', flags)
+    # print('param: ', param)
+    print('event: ', event)
+
+    # if event == cv2.EVENT_LBUTTONDBLCLK:
+    #     param[0] = np.zeros(param[0].shape, np.uint8) + 255
+    #     isDoubleClick = True
+
+    # else:
+
+    #     # if event == cv2.EVENT_LBUTTONDOWN:
+    #     if event == cv2.EVENT_LBUTTONUP: #마우스 왼쪽 클릭
+    #         if isDoubleClick:
+    #             isDoubleClick = False
+    #         elif flags & cv2.EVENT_FLAG_SHIFTKEY: # 시프트 키와 함께
+    #             cv2.rectangle(param[0], (x-5, y-5), (x+5, y+5), (255,0,0))
+    #         else:
+    #             cv2.circle(param[0], (x,y), 5, (255,0,0), 3)
+
+    #     elif event == cv2.EVENT_RBUTTONDOWN: # 마우스 오른쪽 클릭
+    #         cv2.circle(param[0], (x,y), 5, (0,0,255), 3)
+
+        # 1,4,7,(1,4)
+
+    # if event == cv2.EVENT_LBUTTONDOWN: #마우스 왼쪽 클릭
+    if event == cv2.EVENT_LBUTTONUP: #마우스 왼쪽 클릭
+
+        if isDoubleClick:
+            isDoubleClick = False
+        elif flags & cv2.EVENT_FLAG_SHIFTKEY: # 시프트 키와 함께
+            cv2.rectangle(param[0], (x-5, y-5), (x+5, y+5), (255,0,0))
         else:
-            cv2.circle(param[0], (x, y), 5, (255, 0, 0), 3)
+            cv2.circle(param[0], (x,y), 5, (255,0,0), 3)
+
+    elif event == cv2.EVENT_RBUTTONDOWN: # 마우스 오른쪽 클릭
+        cv2.circle(param[0], (x,y), 5, (0,0,255), 3)
         
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        cv2.circle(param[0], (x, y), 5, (0, 0, 255), 3)
-    elif event == cv2.EVENT_LBUTTONDBLCLK:
+    elif event == cv2.EVENT_LBUTTONDBLCLK: # 마우스 왼쪽 버튼 더블클릭
         param[0] = np.zeros(param[0].shape, np.uint8) + 255
-    cv2.imshow("img", param[0])
+        isDoubleClick = True
 
-img = np.zeros((512, 512, 3), np.uint8) + 255
+        
+    cv2.imshow('img', param[0])
+    pass
 
+
+
+#지우고 그리기
+img = np.zeros((512,512,3), dtype=np.uint8) + 255 #지우기
 cv2.imshow('img', img)
 cv2.setMouseCallback('img', onMouse, [img])
-cv2.waitKey()
+
+# cv2.waitKey()
 
 while True:
     key = cv2.waitKey()
     if key == 27:
         break
-    
+
 cv2.destroyAllWindows()
